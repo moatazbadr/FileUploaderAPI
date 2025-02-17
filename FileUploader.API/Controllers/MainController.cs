0f4b15e0e654c170c726950c15fba92b4163a469
moatazbadr <zezobadr988@gmail.com>
2025-02-17 19:25:29 +0200

    initialupload

---
+++ FileUploader.API/Controllers/MainController.cs
@@ -0,0 +1,17 @@
+﻿using FileUploader.API.PublicClasses;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FileUploader.API.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class MainController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult UploadFile(IFormFile formFile)
+        {
+            return Ok (new uploadHandler().Upload(formFile));
+        }
+    }
+}
---
+++ FileUploader.API/PublicClasses/uploadHandler.cs
@@ -0,0 +1,34 @@
+﻿namespace FileUploader.API.PublicClasses
+{
+    public class uploadHandler
+    { 
+
+
+        public string Upload(IFormFile file)
+        {
+            //extension
+            List<string> validExtensions = new List<string>() {".jpg",".gif",".png"};
+            string extension= Path.GetExtension(file.FileName);
+            if (!validExtensions.Contains(extension)) {
+                return $"Extensiton is not valid only valid is ({string.Join(',', validExtensions)}) ";
+            }
+            //FileSize of 10mb
+            long size =file.Length;
+            if (size > (10 * 1024 * 1024)) {
+                return "File size limit exceeded";
+            }
+
+            //name changing
+            string FileName= Guid.NewGuid().ToString();
+            string path=Path.Combine(Directory.GetCurrentDirectory(),"Uploads");
+            //will atomatically close the stream
+            using  FileStream stream = new FileStream(Path.Combine(path,FileName),FileMode.Create);
+            file.CopyTo( stream);    
+            //stream.Dispose();
+            //stream.Close();
+
+            return FileName;
+
+        }
+    }
+}
